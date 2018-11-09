2461597a7875672a0df4f689452310db392575cd
dell <letcafe@outlook.com>
2018-11-09 23:18:02 +0800

    support alipay

--- src/main/java/com/edoctor/Application.java
+++ src/main/java/com/edoctor/Application.java
@@ -1,14 +1,11 @@
 package com.edoctor;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.edoctor")
-@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
---
+++ src/test/java/dbTest/think_in_java/util/Generator.java
@@ -0,0 +1,4 @@
+//: net/mindview/util/Generator.java
+// A generic interface.
+package dbTest.think_in_java.util;
+public interface Generator<T> { T next(); } ///:~
